2c3dcc7e24b1255710942f6aefb22b0a40901c5a
Shukhrat <shukhrat@Shukhrats-MacBook-Pro.local>
2023-10-06 05:24:37 +0100

    update service page responsiveness

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,112 +1,227 @@
 import React from "react";
 import Preloader from "../components/Preloader";
+import { useMediaQuery } from "react-responsive";
 
 export default function Services() {
+  const isTablet = useMediaQuery({ query: "(max-width: 1200px)" });
   return (
     <div className="services">
       <Preloader />
       <div className="services__container">
-        <div className="services__1">
-          <div className="services__1-left">
-            <ul>
-              <li>
-                <i class="icon fa-solid fa-cubes"></i> <span>Photoshop</span>
-              </li>
-              <li>
-                <i class="icon fa-solid fa-code"></i>
-                <span>Wed Development</span>
-              </li>
-              <li>
-                <i class="icon fa-solid fa-desktop"></i>
-                <span>Web Design</span>
-              </li>
-              <li>
-                <i class="icon fa-solid fa-mobile-screen-button"></i>
-                <span>Mobile Development</span>
-              </li>
-            </ul>
-          </div>
-          <div className="services__1-right">
-            <h1>MY OFFERINGS</h1>
-            <div className="services__1-right-services">
-              <div className="services__1-right-box">
-                <h5>Photoshop</h5>
-                <p>
-                  Sit amet luctussd fav venenatis, lectus magna fringilla inis
-                  urna, porttitor asna rhoncus dolor purus non enim aberitin
-                  praesent in elementum sahas facilisis leo, vel fringilla est
-                  etisam dignissim.
-                </p>
+        {isTablet ? (
+          <>
+            <div className="services__1">
+              <div className="services__1-right">
+                <h1>MY OFFERINGS</h1>
+                <div className="services__1-right-services">
+                  <div className="services__1-right-box">
+                    <h5>Photoshop</h5>
+                    <p>
+                      Sit amet luctussd fav venenatis, lectus magna fringilla
+                      inis urna, porttitor asna rhoncus dolor purus non enim
+                      aberitin praesent in elementum sahas facilisis leo, vel
+                      fringilla est etisam dignissim.
+                    </p>
+                  </div>
+                  <div className="services__1-right-box">
+                    <h5>Web Development</h5>
+                    <p>
+                      Sit amet luctussd fav venenatis, lectus magna fringilla
+                      inis urna, porttitor asna rhoncus dolor purus non enim
+                      aberitin praesent in elementum sahas facilisis leo, vel
+                      fringilla est etisam dignissim.
+                    </p>
+                  </div>
+                  <div className="services__1-right-box">
+                    <h5>Web Design</h5>
+                    <p>
+                      Sit amet luctussd fav venenatis, lectus magna fringilla
+                      inis urna, porttitor asna rhoncus dolor purus non enim
+                      aberitin praesent in elementum sahas facilisis leo, vel
+                      fringilla est etisam dignissim.
+                    </p>
+                  </div>
+                  <div className="services__1-right-box">
+                    <h5>Mobile Development</h5>
+                    <p>
+                      Sit amet luctussd fav venenatis, lectus magna fringilla
+                      inis urna, porttitor asna rhoncus dolor purus non enim
+                      aberitin praesent in elementum sahas facilisis leo, vel
+                      fringilla est etisam dignissim.
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="services__1-right-box">
-                <h5>Web Development</h5>
-                <p>
-                  Sit amet luctussd fav venenatis, lectus magna fringilla inis
-                  urna, porttitor asna rhoncus dolor purus non enim aberitin
-                  praesent in elementum sahas facilisis leo, vel fringilla est
-                  etisam dignissim.
-                </p>
-              </div>
-              <div className="services__1-right-box">
-                <h5>Web Design</h5>
-                <p>
-                  Sit amet luctussd fav venenatis, lectus magna fringilla inis
-                  urna, porttitor asna rhoncus dolor purus non enim aberitin
-                  praesent in elementum sahas facilisis leo, vel fringilla est
-                  etisam dignissim.
-                </p>
-              </div>
-              <div className="services__1-right-box">
-                <h5>Mobile Development</h5>
-                <p>
-                  Sit amet luctussd fav venenatis, lectus magna fringilla inis
-                  urna, porttitor asna rhoncus dolor purus non enim aberitin
-                  praesent in elementum sahas facilisis leo, vel fringilla est
-                  etisam dignissim.
-                </p>
+            </div>
+            <div className="services__1-left">
+              <ul>
+                <li>
+                  <i class="icon fa-solid fa-cubes"></i> <span>Photoshop</span>
+                </li>
+                <li>
+                  <i class="icon fa-solid fa-code"></i>
+                  <span>Wed Development</span>
+                </li>
+                <li>
+                  <i class="icon fa-solid fa-desktop"></i>
+                  <span>Web Design</span>
+                </li>
+                <li>
+                  <i class="icon fa-solid fa-mobile-screen-button"></i>
+                  <span>Mobile Development</span>
+                </li>
+              </ul>
+            </div>
+          </>
+        ) : (
+          <div className="services__1">
+            <div className="services__1-left">
+              <ul>
+                <li>
+                  <i class="icon fa-solid fa-cubes"></i> <span>Photoshop</span>
+                </li>
+                <li>
+                  <i class="icon fa-solid fa-code"></i>
+                  <span>Wed Development</span>
+                </li>
+                <li>
+                  <i class="icon fa-solid fa-desktop"></i>
+                  <span>Web Design</span>
+                </li>
+                <li>
+                  <i class="icon fa-solid fa-mobile-screen-button"></i>
+                  <span>Mobile Development</span>
+                </li>
+              </ul>
+            </div>
+            <div className="services__1-right">
+              <h1>MY OFFERINGS</h1>
+              <div className="services__1-right-services">
+                <div className="services__1-right-box">
+                  <h5>Photoshop</h5>
+                  <p>
+                    Sit amet luctussd fav venenatis, lectus magna fringilla inis
+                    urna, porttitor asna rhoncus dolor purus non enim aberitin
+                    praesent in elementum sahas facilisis leo, vel fringilla est
+                    etisam dignissim.
+                  </p>
+                </div>
+                <div className="services__1-right-box">
+                  <h5>Web Development</h5>
+                  <p>
+                    Sit amet luctussd fav venenatis, lectus magna fringilla inis
+                    urna, porttitor asna rhoncus dolor purus non enim aberitin
+                    praesent in elementum sahas facilisis leo, vel fringilla est
+                    etisam dignissim.
+                  </p>
+                </div>
+                <div className="services__1-right-box">
+                  <h5>Web Design</h5>
+                  <p>
+                    Sit amet luctussd fav venenatis, lectus magna fringilla inis
+                    urna, porttitor asna rhoncus dolor purus non enim aberitin
+                    praesent in elementum sahas facilisis leo, vel fringilla est
+                    etisam dignissim.
+                  </p>
+                </div>
+                <div className="services__1-right-box">
+                  <h5>Mobile Development</h5>
+                  <p>
+                    Sit amet luctussd fav venenatis, lectus magna fringilla inis
+                    urna, porttitor asna rhoncus dolor purus non enim aberitin
+                    praesent in elementum sahas facilisis leo, vel fringilla est
+                    etisam dignissim.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="about__3">
-          <div className="home__2-3">
-            <div className="home__2-3-social">
-              <i class="home__2-3-social-icon fa-brands fa-linkedin-in"></i>
-              <i class="home__2-3-social-icon fa-brands fa-github"></i>
-            </div>
-            <div className="home__2-3-title">
-              <div>
-                <span>STAY WITH ME</span>
-                <h2>Profiles</h2>
+        )}
+
+        {isTablet ? (
+          <>
+            <div className="about__tablet">
+              <div className="home__2-3">
+                <div className="home__2-3-social">
+                  <i class="home__2-3-social-icon fa-brands fa-linkedin-in"></i>
+                  <i class="home__2-3-social-icon fa-brands fa-github"></i>
+                </div>
+                <div className="home__2-3-title">
+                  <div>
+                    <span>STAY WITH ME</span>
+                    <h2>Profiles</h2>
+                  </div>
+                  <div>
+                    <img src="/images/logo.png" alt="" />
+                  </div>
+                </div>
               </div>
-              <div>
-                <img src="/images/logo.png" alt="" />
+
+              <div className="home__1-2-2-1">
+                <div className="home__1-2-2-1-sign">
+                  <img src="/images/sign.png" alt="" />
+                </div>
+                <div className="home__1-2-2-1-title">
+                  <div>
+                    <span>MORE ABOUT ME</span>
+                    <h2>Credentials</h2>
+                  </div>
+                  <div>
+                    <img src="/images/logo.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="home__3-2">
+              <i class="home__3-2-icon fa-solid fa-paper-plane"></i>
+              <h1>
+                Let's <br />
+                work <span>together</span>
+              </h1>
+              <img src="/images/logo.png" alt="" />
+            </div>
+          </>
+        ) : (
+          <div className="about__3">
+            <div className="home__2-3">
+              <div className="home__2-3-social">
+                <i class="home__2-3-social-icon fa-brands fa-linkedin-in"></i>
+                <i class="home__2-3-social-icon fa-brands fa-github"></i>
+              </div>
+              <div className="home__2-3-title">
+                <div>
+                  <span>STAY WITH ME</span>
+                  <h2>Profiles</h2>
+                </div>
+                <div>
+                  <img src="/images/logo.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="home__3-2">
+              <i class="home__3-2-icon fa-solid fa-paper-plane"></i>
+              <h1>
+                Let's <br />
+                work <span>together</span>
+              </h1>
+              <img src="/images/logo.png" alt="" />
+            </div>
+            <div className="home__1-2-2-1">
+              <div className="home__1-2-2-1-sign">
+                <img src="/images/sign.png" alt="" />
+              </div>
+              <div className="home__1-2-2-1-title">
+                <div>
+                  <span>MORE ABOUT ME</span>
+                  <h2>Credentials</h2>
+                </div>
+                <div>
+                  <img src="/images/logo.png" alt="" />
+                </div>
               </div>
             </div>
           </div>
-          <div className="home__3-2">
-            <i class="home__3-2-icon fa-solid fa-paper-plane"></i>
-            <h1>
-              Let's <br />
-              work <span>together</span>
-            </h1>
-            <img src="/images/logo.png" alt="" />
-          </div>
-          <div className="home__1-2-2-1">
-            <div className="home__1-2-2-1-sign">
-              <img src="/images/sign.png" alt="" />
-            </div>
-            <div className="home__1-2-2-1-title">
-              <div>
-                <span>MORE ABOUT ME</span>
-                <h2>Credentials</h2>
-              </div>
-              <div>
-                <img src="/images/logo.png" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
