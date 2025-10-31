bb2dba9eb5148c352fc96b7187d59a2911aeb357
Shukhrat <shukhratmamadaliev9797@gmail.com>
2025-10-31 17:47:00 +0500

    added projects

---
+++ src/data.js
@@ -0,0 +1,88 @@
+const data = {
+    projects: {
+        real: [{
+            name: 'Assalaam UK - Charity',
+            url: 'https://www.assalaamuk.org/',
+            img: '/images/projects/assalaam.png',
+            github: 'fds',
+            technologies: ['React', 'Redux', 'Sass', 'react-bootstrap', 'node', 'expressjs', 'mongo', 'Stripe'],
+            description: "charity-focused web project – a purpose-built platform designed to inspire positive change. With a user-friendly interface, users can easily contribute to various donation projects, stay informed about ongoing charity activities through news updates, and track the progress of their donations.This fully functional website controlled by admins to efficiently manage the entire platform, ensuring a seamless experience for both donors and beneficiaries."
+        },
+        {
+            name: 'Proactiveuz - Study Abroad Agency',
+            url: 'https://proactiveuz.com/',
+            img: '/images/projects/proactiveuz.png',
+            technologies: ['React', 'Redux', 'Sass', 'node', 'expressjs', 'mongo'],
+            description: 'This project involves creating a user-friendly web app for a study abroad consulting agency. Users can explore detailed university information, stay updated on scholarship news, and effortlessly connect with the agency for personalized guidance. The goal is to streamline the study abroad process and provide a convenient platform for users to access essential information.'
+        },],
+        personal: [{
+            name: "Suxbat",
+            url: "https://suxbat-ea74802884a2.herokuapp.com/",
+            img: "/images/projects/suxbat.png",
+            technologies: ['React', 'Sass'],
+            github: 'https://github.com/Shukhratmamadaliev9797/suxbat',
+            description: "Suxbat is a social media web application that allows users to share short text posts, follow other users, and engage with content through likes and comments. The platform emphasizes simplicity and ease of use, providing a seamless experience for users to connect and share their thoughts in a concise format."
+        }, {
+            name: "Eatturkish",
+            url: 'https://eatturkish-af669971ee9a.herokuapp.com/',
+            img: '/images/projects/eatturkish.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/Eatturkish',
+            description: 'This project focuses on building a modern and user-friendly website for a Turkish restaurant. Visitors can explore the full menu with detailed dish information, check updated prices, and browse mouth-watering food photos. The platform allows users to place online orders, make table reservations, and stay informed about special offers or new menu items. The goal is to provide a smooth and enjoyable browsing experience while helping customers easily connect with the restaurant.'
+        }, {
+            name: "Bepul",
+            url: 'https://bepul-eb7dff9b239a.herokuapp.com/',
+            img: '/images/projects/bepul.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/Bepul',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        },
+        {
+            name: "Old Portofolio",
+            url: 'https://portofolio-shukhrat.netlify.app/',
+            img: '/images/projects/porto1.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/shukhrat.com',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        }, {
+            name: "Global news",
+            url: 'https://shrieking-hollow-68252-b5750eee12d2.herokuapp.com/',
+            img: '/images/projects/news.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/blog-app',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        }, {
+            name: "YouTube clone",
+            url: 'https://shukhrat-youtube-app.netlify.app/',
+            img: '/images/projects/youtube.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/youtube-app',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        },
+        {
+            name: "Old Portofolio",
+            url: 'https://shukhrats-cv.netlify.app/',
+            img: '/images/projects/porto2.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/shukhrats.com',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        },
+        {
+            name: "Weather app",
+            url: 'https://shukhrat-weather-app.netlify.app/',
+            img: '/images/projects/weather.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/weather.io',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        }, {
+            name: "Photo finder",
+            url: 'https://shukhrat-unsplash-app.netlify.app/',
+            img: '/images/projects/unsplash.png',
+            technologies: ['React', "Sass"],
+            github: 'https://github.com/Shukhratmamadaliev9797/weather.io',
+            description: 'This project focuses on building a user-friendly platform where people can give away items for free. Users can easily post things they no longer need, add photos and descriptions, and categorize their listings for better visibility. Visitors can browse available items, search by location, and contact owners to arrange pickup. The goal is to reduce waste, encourage community sharing, and provide a simple way for people to pass on useful items instead of throwing them away.'
+        }]
+
+    },
+}
+export default data;
